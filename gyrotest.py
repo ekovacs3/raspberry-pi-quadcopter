7503054093fcad40caa5c1831ffec5f2494470e5
Evan Kovacs <ekovacs3.15k@gmail.com>
2014-05-15 19:54:28 +0000

    Added try/except to prevent crashing with bad data

--- gyrotest.py
+++ gyrotest.py
@@ -2,19 +2,28 @@
 
 import smbus
 import math
+import time
 
 # Power management registers
 power_mgmt_1 = 0x6b
 power_mgmt_2 = 0x6c
 
 def read_byte(adr):
-    return bus.read_byte_data(address, adr)
+        try:
+                return bus.read_byte_data(address, adr)
+        except IOError:
+                print "there was an IOError"
+		return 0
 
 def read_word(adr):
-    high = bus.read_byte_data(address, adr)
-    low = bus.read_byte_data(address, adr+1)
-    val = (high << 8) + low
-    return val
+        try:
+            high = bus.read_byte_data(address, adr)
+            low = bus.read_byte_data(address, adr+1)
+            val = (high << 8) + low
+            return val
+        except IOError:
+                print "There was an IO Error"
+		return 0 
 
 def read_word_2c(adr):
     val = read_word(adr)
@@ -37,35 +46,48 @@ def get_x_rotation(x,y,z):
 bus = smbus.SMBus(1) # or bus = smbus.SMBus(1) for Revision 2 boards
 address = 0x68       # This is the address value read via the i2cdetect command
 
+
 # Now wake the 6050 up as it starts in sleep mode
-bus.write_byte_data(address, power_mgmt_1, 0)
+startprogram = False
+while startprogram != True:
+	try:
+        	bus.write_byte_data(address, power_mgmt_1, 0)
+        	startprogram = True
+	except IOError:
+		print "there was an IOError"
+	time.sleep(.5)
 
-print "gyro data"
-print "---------"
 
-gyro_xout = read_word_2c(0x43)
-gyro_yout = read_word_2c(0x45)
-gyro_zout = read_word_2c(0x47)
-
-print "gyro_xout: ", gyro_xout, " scaled: ", (gyro_xout / 131)
-print "gyro_yout: ", gyro_yout, " scaled: ", (gyro_yout / 131)
-print "gyro_zout: ", gyro_zout, " scaled: ", (gyro_zout / 131)
-
-print
-print "accelerometer data"
-print "------------------"
-
-accel_xout = read_word_2c(0x3b)
-accel_yout = read_word_2c(0x3d)
-accel_zout = read_word_2c(0x3f)
-
-accel_xout_scaled = accel_xout / 16384.0
-accel_yout_scaled = accel_yout / 16384.0
-accel_zout_scaled = accel_zout / 16384.0
-
-print "accel_xout: ", accel_xout, " scaled: ", accel_xout_scaled
-print "accel_yout: ", accel_yout, " scaled: ", accel_yout_scaled
-print "accel_zout: ", accel_zout, " scaled: ", accel_zout_scaled
-
-print "x rotation: " , get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
-print "y rotation: " , get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
+while True:
+	        
+	print "gyro data"
+	print "---------"
+	
+	gyro_xout = read_word_2c(0x43)
+	gyro_yout = read_word_2c(0x45)
+	gyro_zout = read_word_2c(0x47)
+	
+	print "gyro_xout: ", gyro_xout, " scaled: ", (gyro_xout / 131)
+	print "gyro_yout: ", gyro_yout, " scaled: ", (gyro_yout / 131)
+	print "gyro_zout: ", gyro_zout, " scaled: ", (gyro_zout / 131)
+	
+	print
+	print "accelerometer data"
+	print "------------------"
+	
+	accel_xout = read_word_2c(0x3b)
+	accel_yout = read_word_2c(0x3d)
+	accel_zout = read_word_2c(0x3f)
+	
+	accel_xout_scaled = accel_xout / 16384.0
+	accel_yout_scaled = accel_yout / 16384.0
+	accel_zout_scaled = accel_zout / 16384.0
+	
+	print "accel_xout: ", accel_xout, " scaled: ", accel_xout_scaled
+	print "accel_yout: ", accel_yout, " scaled: ", accel_yout_scaled
+	print "accel_zout: ", accel_zout, " scaled: ", accel_zout_scaled
+	
+	print "x rotation: " , get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
+	print "y rotation: " , get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
+	
+	time.sleep(.5)

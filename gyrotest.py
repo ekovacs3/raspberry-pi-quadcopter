db43eb6245d6bd94e5607ed10c21ecfafd86df68
Evan Kovacs <ekovacs3.15k@gmail.com>
2014-07-14 14:15:06 +0000

    added mpu addresses and started porting to python

--- gyrotest.py
+++ gyrotest.py
@@ -34,7 +34,7 @@ def read_word_2c(adr):
 
 def dist(a,b):
     return math.sqrt((a*a)+(b*b))
-
+ 
 def get_y_rotation(x,y,z):
     radians = math.atan2(x, dist(y,z))
     return -math.degrees(radians)
